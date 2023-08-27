235b14d5ac302f8247ff5bf6f78da1f92a1abf39
bia <beatrizmds2005@gmail.com>
2023-08-27 16:43:46 -0300

    layout site

--- index.php
+++ index.php
@@ -9,48 +9,50 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <!-- ESTILIZAÇÃO INTERNA -->
+    <style type="text/css">
+        .navbar{
+            margin-bottom: 0;
+            padding: 1rem;
+            border-radius: 0; 
+        }
+    </style>
 </head>
 <body>
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">NerdFigure</a>
-    </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="#">Home<span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Lançamentos</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categorias <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Naruto</a></li>
-            <li><a href="#">One Piece</a></li>
-            <li><a href="#">Avangers</a></li>
-            <li><a href="#">Disney</a></li>
-            <li><a href="#">Friends</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="O que você procura?">
+  <?php include 'nav.php'; ?>
+  <?php include 'cabecalho.html'; ?>
+
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-3">
+              <img src="https://placehold.it/450x320" class="img-responsive" style="width: 100%">
+              <div><h1>Nome do produto</h1></div>
+              <div><h4>R$220,00</h4></div>
+            </div>
+
+            <div class="col-sm-3">
+              <img src="https://placehold.it/450x320" class="img-responsive" style="width: 100%">
+              <div><h1>Nome do produto</h1></div>
+              <div><h4>R$220,00</h4></div>
+            </div>
+
+            <div class="col-sm-3">
+              <img src="https://placehold.it/450x320" class="img-responsive" style="width: 100%">
+              <div><h1>Nome do produto</h1></div>
+              <div><h4>R$220,00</h4></div>
+            </div>
+
+            <div class="col-sm-3">
+              <img src="https://placehold.it/450x320" class="img-responsive" style="width: 100%">
+              <div><h1>Nome do produto</h1></div>
+              <div><h4>R$220,00</h4></div>
+            </div>
+
+          </div>
         </div>
-        <button type="submit" class="btn btn-default">Pesquisar</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Contato</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"> Login</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+
+
+        <?php include 'rodape.html'; ?>
 </body>
 </html>
